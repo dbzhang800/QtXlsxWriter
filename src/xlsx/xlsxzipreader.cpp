2f1aef64159ebfc6b77a2d8d08e6869554304f66
Lyndon33 <biglyndon@outlook.com>
2016-07-29 23:59:49 +0800

    Update xlsxzipreader.cpp
    For working on Qt5.6

--- src/xlsx/xlsxzipreader.cpp
+++ src/xlsx/xlsxzipreader.cpp
@@ -27,6 +27,8 @@
 
 #include <private/qzipreader_p.h>
 
+#include <QVector>
+
 namespace QXlsx {
 
 ZipReader::ZipReader(const QString &filePath) :
@@ -48,7 +50,7 @@ ZipReader::~ZipReader()
 
 void ZipReader::init()
 {
-    QList<QZipReader::FileInfo> allFiles = m_reader->fileInfoList();
+    QVectort<QZipReader::FileInfo> allFiles = m_reader->fileInfoList();
     foreach (const QZipReader::FileInfo &fi, allFiles) {
         if (fi.isFile)
             m_filePaths.append(fi.filePath);
