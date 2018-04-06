57199a61f56aef1f56c5179650ad775066226817
Михаил Верисокин <drow.it@gmail.com>
2018-04-06 10:53:09 +0300

    Coding style changed

--- src/xlsx/xlsxrichstring.cpp
+++ src/xlsx/xlsxrichstring.cpp
@@ -166,26 +166,26 @@ QString RichString::toHtml() const
 */
 void RichString::setHtml(const QString &text)
 {
-	QTextDocument doc;
-	doc.setHtml(text);
+    QTextDocument doc;
+    doc.setHtml(text);
 
-	QTextBlock block = doc.firstBlock();
-	while (block.isValid())
-	{
-		QTextBlock::iterator it;
-		for (it = block.begin(); !(it.atEnd()); ++it) {
-			QTextFragment textFragment = it.fragment();
-			if (textFragment.isValid()) {
-				Format fmt;
-				fmt.setFont(textFragment.charFormat().font());
-				fmt.setFontColor(textFragment.charFormat().foreground().color());
-				addFragment(textFragment.text(), fmt);
-			}
-		}
+    QTextBlock block = doc.firstBlock();
+    while (block.isValid()) {
+        QTextBlock::iterator it;
+        for (it = block.begin(); !(it.atEnd()); ++it) {
+            QTextFragment textFragment = it.fragment();
+            if (textFragment.isValid()) {
+                Format fmt;
+                fmt.setFont(textFragment.charFormat().font());
+                fmt.setFontColor(textFragment.charFormat().foreground().color());
+                addFragment(textFragment.text(), fmt);
+            }
+        }
 
-		block = block.next();
-		if (block.isValid()) addFragment("\n", Format());
-	}
+        block = block.next();
+        if (block.isValid()) 
+            addFragment("\n", Format());
+    }
 }
 
 /*!
